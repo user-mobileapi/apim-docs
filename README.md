45e03652e915c45f132534a32e7f0a4e2556e282
chanthakim15 <chantha.kd@gmail.com>
2015-07-15 11:10:23 -0500

    cleaned up

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # apim-docs
 
-Note :
-
-convert.js and layout.html/indexLayout.html should be in the same folder or there will an error
--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-/**
- * Created by chkim on 7/9/2015.
- */
 
-var lib = require('./api/docs/lib/converter.js');
-
-//var indexLayoutFile = 'api/docs/lib/indexLayout.html';
-//var contentLayoutFile = 'api/docs/lib/layout.html';
-//var  markdownDir= 'api/docs/md';
-//var  htmlDir= 'api/docs/html';
-//var indexHtmlDir='api/docs';
+var lib = require('./lib/converter.js');
 
 //InitDocs('destination');
-
 function InitDocs(destination) {
-
-    // copy all api folders to api folder
     lib.copyDocsToApiDirectory(destination);
-
-    // generate html file
-   // lib.convertMarkdownsToHtmls();
 }
-
-// copy files and folders in docs to the current working directory /api
 
 module.exports ={
     InitDocs: InitDocs
